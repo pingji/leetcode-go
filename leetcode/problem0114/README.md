566fab1f401bb51424b3568591cbbeec9d3f98a5
pingji <jiping_1985@163.com>
2021-04-07 01:34:36 +0800

    solved 114

--- leetcode/problem0114/README.md
+++ leetcode/problem0114/README.md
@@ -1,3 +1,7 @@
 ## Solutions
 ### Solution 1
-递归
+递归。 参考：[手把手带你刷二叉树（第一期）](https://labuladong.gitbook.io/algo/shu-ju-jie-gou-xi-lie/shou-ba-shou-shua-er-cha-shu-xun-lian-di-gui-si-wei/er-cha-shu-xi-lie-1)
+- 时间复杂度：O(n)
+- 空间复杂度：O(n)
+  
+![flatten](https://gblobscdn.gitbook.com/assets%2F-MWvhB2heCSJoT6IpxDY%2Fsync%2Fd11ee940259be5f710700e2ed073068a64bf5746.jpeg?alt=media))
--- leetcode/problem0114/solution/solution.go
+++ leetcode/problem0114/solution/solution.go
@@ -7,5 +7,23 @@ type TreeNode struct {
 }
 
 func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	// 1、左右子树已经被拉平成一条链表
+	flatten(root.Left)
+	flatten(root.Right)
+	left := root.Left
+	right := root.Right
 
+	// 2、将左子树作为右子树
+	root.Right = left
+	root.Left = nil
+
+	// 3、将原先的右子树接到当前右子树的末端
+	node := root
+	for node.Right != nil {
+		node = node.Right
+	}
+	node.Right = right
 }
